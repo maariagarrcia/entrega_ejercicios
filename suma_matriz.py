343e7fcc00ba20ccea73ced3cc95e54a4abe1702
maariagarrcia <mrrmn@myuax.com>
2021-12-04 11:02:35 +0100

    import

--- suma_matriz.py
+++ suma_matriz.py
@@ -1,6 +1,16 @@
+import math
+# Proporciona funciones de interfaz funcionales para la mayoría de los sistemas operativos
+import os
+import random
+# Uso de las expresiones regualares que son  una secuencia de caracteres que forma un patrón de búsqueda
+import re
+# Este módulo proporciona acceso a variables utilizadas o mantenidas por el intérprete y 
+# a funciones que interactúan estrechamente con el intérprete
+import sys
+# Se usa para trabajar con arrays
 import numpy as np 
 
-# Creacion de una lista (array aunque pyhton no tenga soporte de ello).
+# Creacion de una lista (array aunque pyhton no tenga soporte de ello)
 # Sumatorio de los numeros de la matriz
 def simpleArraysum(ar):
     suma=0
