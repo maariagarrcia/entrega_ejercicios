460a601b5f7a99040713c7ee6f6c0ee192e31704
maariagarrcia <mrrmn@myuax.com>
2021-12-04 10:48:46 +0100

    array

---
+++ suma_matriz.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+# Creacion de una lista (array aunque pyhton no tenga soporte de ello).
+# Sumatorio de los numeros de la matriz
+def simpleArraysum(ar):
+    suma=0
+    for x in ar:
+        suma= suma + x
+        #print(x)
+    return suma
+
+
+
+
+    
